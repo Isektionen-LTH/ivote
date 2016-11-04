8f4941322aac91752da87cf9a40e935ed4f38002
John <johnfarnes@hotmail.com>
2016-11-04 17:42:29 +0100

    Added simple server side login logix

---
+++ app/login.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const router = express.Router();
+
+const credentials = require('./credentials.json');
+const salt = 'ivote';
+
+router.get('/voter/:id', function(req, res) {
+	res.cookie({
+		voteId: req.params.id
+	});
+	res.redirect('/vote');
+});
+
+router.get('/admin', function(req, res) {
+	if(matchingCredentials(req.cookie, credentials.admin)) {
+		res.redirect('/admin');
+	} else {
+		res.redirect('/login/register');
+	}
+})
+
+router.get('/register', function(req, res) {
+	if(matchingCredentials(req.cookie, credentials.register)) {
+		res.redirect('/register');
+	} else {
+		res.redirect('/login/register');
+	}
+	
+});
+
+function auth(req, res, next) {
+	if(req.cookie.voteId) {
+		req.voteId = req.cookie.voteId;
+		req.role = 'voter';
+	} else if(req.cookie.username && req.cookie.hash) {
+		if(matchingCredentials(req.cookie, credentials.admin)) {
+			req.role = 'admin';
+		} else if(matchingCredentials(req.cookie, credentials.register)) {
+			req.role = 'register';
+		}
+	}
+	next();
+}
+
+function matchingCredentials(cookie, credentials) {
+	return cookie.username === credentials.username
+		&& cookie.hash === hash(credentials.password + salt);
+}
+
+function hash(string) {
+  var hash = 0, i, chr, len;
+  if (string.length === 0) return hash;
+  for (i = 0, len = string.length; i < len; i++) {
+    chr   = string.charCodeAt(i);
+    hash  = ((hash << 5) - hash) + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return hash;
+};
+
+module.exports = {
+	router: router,
+	auth: auth
+};
