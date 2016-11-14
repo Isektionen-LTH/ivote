c563e0e2e40b17d8f19e97c16b74b79702e40908
John Rapp <johnfarnes@hotmail.com>
2016-11-14 01:04:03 +0100

    Removed link to results

--- client/app/components/index.js
+++ client/app/components/index.js
@@ -20,9 +20,6 @@ export default function IndexPage() {
 				<Link to={'/vote'}>Rösta</Link>
 			</div>
 			<div>
-				<Link to={'/results'}>Se resultat</Link>
-			</div>
-			<div>
 				<Link to={'/login'}>Logga in som admin</Link>
 			</div>
 		</Paper>
