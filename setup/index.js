8a70bb3e805813e7d9ba97b6435d192b73a592cf
John Rapp <johnfarnes@hotmail.com>
2017-11-10 17:17:35 +0100

    "Setup successfull!"

--- setup/index.js
+++ setup/index.js
@@ -32,43 +32,6 @@ inquirer.prompt(questions).then(function (answers) {
 
   fs.writeFileSync('../credentials.json', JSON.stringify(credentials, null, '\t'));
   fs.writeFileSync('../config.json', JSON.stringify(config, null, '\t'));
+
+  console.log('Successfully set up!');
 });
-
-/* ES6
-const inquirer = require('inquirer');
-const fs = require('fs');
-
-const questions = require('./questions');
-
-inquirer.prompt(questions).then((answers) => {
-  const {
-    adminUsername,
-    adminPassword,
-    registerUsername,
-    registerPassword,
-    emailUsername,
-    emailPassword,
-    dbUrl,
-    url
-  } = answers;
-
-  const credentials = {
-    admin: {
-      username: adminUsername,
-      password: adminPassword
-    },
-    register: {
-      username: registerUsername,
-      password: registerPassword
-    },
-    email: {
-      username: emailUsername,
-      password: emailPassword
-    }
-  };
-  const config = { dbUrl, url };
-
-  fs.writeFileSync('../credentials.json', JSON.stringify(credentials, null, '\t'));
-  fs.writeFileSync('../config.json', JSON.stringify(config, null, '\t'));
-});
-*/
